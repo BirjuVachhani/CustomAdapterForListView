75c486a412c3aea3fc7e57eeade89b824f79be46
birju.vachhani <brvachhani@gmail.com>
2018-01-19 13:06:03 +0530

    Create CustomAdapter java class

---
+++ app/src/main/java/com/birjuvachhani/customadapterforlistview/CustomAdapter.java
@@ -0,0 +1,52 @@
+package com.birjuvachhani.customadapterforlistview;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by birju.vachhani on 19/01/18.
+ */
+
+public class CustomAdapter extends ArrayAdapter {
+
+    private Context context;
+    private String []titles;
+    private String []desc;
+    private int []images;
+    private int singleRowLayoutId;
+
+    public CustomAdapter(Context context, int singleRowLayoutId, String []titles, String []desc, int []images ) {
+        super(context, singleRowLayoutId);
+        this.context=context;
+        this.titles=titles;
+        this.desc=desc;
+        this.images=images;
+        this.singleRowLayoutId=singleRowLayoutId;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        LayoutInflater inflater=(LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        //view object of single row of list view
+        View row=inflater.inflate(singleRowLayoutId,parent,false);
+
+        ImageView mimg=(ImageView)row.findViewById(R.id.iv_img);
+        TextView mtitle=(TextView)row.findViewById(R.id.tv_title);
+        TextView mdesc=(TextView)row.findViewById(R.id.tv_desc);
+
+        mimg.setImageResource(images[position]);
+        mtitle.setText(titles[position]);
+        mdesc.setText(desc[position]);
+
+        return row;
+    }
+}
