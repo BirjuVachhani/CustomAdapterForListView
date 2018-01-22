49e7cf2fe41b404b38f4e39cecbfa44996ef72e0
birju.vachhani <brvachhani@gmail.com>
2018-01-22 14:18:06 +0530

    Add thumbnails for fast image loading

--- app/src/main/java/com/birjuvachhani/customadapterforlistview/MainActivity.java
+++ app/src/main/java/com/birjuvachhani/customadapterforlistview/MainActivity.java
@@ -18,6 +18,11 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                             R.drawable.img11,R.drawable.img12,R.drawable.img13,R.drawable.img14,R.drawable.img15,
                             R.drawable.img16,R.drawable.img17,R.drawable.img18,R.drawable.img19,R.drawable.img20};
 
+    private int []thumbs={R.drawable.thmb1,R.drawable.thmb2,R.drawable.thmb3,R.drawable.thmb4,R.drawable.thmb5,
+            R.drawable.thmb6,R.drawable.thmb7,R.drawable.thmb8,R.drawable.thmb9,R.drawable.thmb10,
+            R.drawable.thmb11,R.drawable.thmb12,R.drawable.thmb13,R.drawable.thmb14,R.drawable.thmb15,
+            R.drawable.thmb16,R.drawable.thmb17,R.drawable.thmb18,R.drawable.thmb19,R.drawable.thmb20};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +33,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         titles=getResources().getStringArray(R.array.titles);
         desc=getResources().getStringArray(R.array.descriptions);
 
-        CustomAdapter adapter=new CustomAdapter(MainActivity.this,R.layout.layout_single_row,titles,desc,images);
+        CustomAdapter adapter=new CustomAdapter(MainActivity.this,R.layout.layout_single_row,titles,desc,thumbs);
         mlist.setAdapter(adapter);
         mlist.setOnItemClickListener(this);
     }
